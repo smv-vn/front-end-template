34770b427eb818b5e294ac88900eba8e15388419
dungtt <trinhtiendung54@gmail.com>
2025-06-10 08:51:30 +0700

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,7 @@
 import { Routes } from '@angular/router';
 import { DemoComponentComponent } from './pages/demo-component/demo-component.component';
-import { PartnerListComponent } from './pages/partner-list/partner-list.component';
 
 export const routes: Routes = [
     { path: 'demo', component: DemoComponentComponent },
-    { path: 'partners', component: PartnerListComponent },
     { path: '', redirectTo: '/partners', pathMatch: 'full' },
 ];
